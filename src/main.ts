144047fec67f068c1eae8d8ec6e0b21f420b22e3
Guy haiby <ghaiby@ucsc.edu>
2024-10-17 14:21:33 -0700

    Step 1 Complete

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,14 @@ const APP_NAME = "Sticker SketchPad";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+// app.innerHTML = APP_NAME;
+
+const title = document.createElement('h1');
+title.textContent = "Sticker SketchPad";
+app.appendChild(title);
+
+const canvas = document.createElement('canvas');
+canvas.width = 256;
+canvas.height = 256;
+canvas.id = "canvas";
+app.appendChild(canvas);
