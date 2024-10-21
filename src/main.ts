3b7b0718a78d9144ac4d2fd1b7f35ddb68d62557
Guy haiby <ghaiby@ucsc.edu>
2024-10-20 23:02:24 -0700

    Step 3 Complete: Add points Array and dispatch event

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,12 @@ const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
 
-let isDrawing = false;
-let X = 0;
-let Y = 0;
+interface Point {
+    x: number;
+    y: number;
+}
+
+const cursor = { isDrawing: false, points: [] as Point[] };
 
 // Title
 const title = document.createElement('h1');
@@ -20,53 +23,67 @@ canvas.width = 256;
 canvas.height = 256;
 canvas.id = "canvas";
 app.appendChild(canvas);
+const ctx = canvas.getContext("2d");
 
 // Add mouse event listeners for drawing
 canvas.addEventListener('mousedown', (event) => startDrawing(event, canvas));
 canvas.addEventListener('mousemove', (event) => draw(event, canvas));
 canvas.addEventListener('mouseup', stopDrawing);
 canvas.addEventListener('mouseout', stopDrawing);
+canvas.addEventListener("drawing-changed", drawingChanged);
+
+// Dispatch drawing changed event on canvas 
+function dispatchDrawingChanged() {
+    const event = new Event("drawing-changed");
+    canvas.dispatchEvent(event);
+}
+
+function addPoint(x: number, y: number) {
+    const newPoint: Point = { x, y };
+    cursor.points.push(newPoint);
+    dispatchDrawingChanged();
+}
 
 function startDrawing(event: MouseEvent, canvas: HTMLCanvasElement) {
-    isDrawing = true;
-    const ctx = canvas.getContext('2d');
-    if (ctx) {
-        ctx.strokeStyle = 'black'; //color
-        ctx.lineWidth = 2;
-        ctx.lineCap = 'round';
-
-        X = event.offsetX;
-        Y = event.offsetY;
-    }
+    cursor.isDrawing = true;
+    cursor.points = [];
+    addPoint(event.offsetX, event.offsetY);
 }
 
 function draw(event: MouseEvent, canvas: HTMLCanvasElement) {
-    if (!isDrawing) return;
-    
-    const ctx = canvas.getContext('2d');
-    if (ctx) {
-        ctx.beginPath();
-        ctx.moveTo(X, Y);
-        ctx.lineTo(event.offsetX, event.offsetY);
-        ctx.stroke();
+    if (!cursor.isDrawing) return;
 
-        X = event.offsetX;
-        Y = event.offsetY;
-    }
+    addPoint(event.offsetX, event.offsetY);
 }
 
 function stopDrawing() {
-    isDrawing = false;
+    cursor.isDrawing = false;
 }
 
-// Add clear 
+function drawingChanged() {
+    if (!ctx) return;
+
+    ctx.strokeStyle = 'black'; 
+    ctx.lineWidth = 2; 
+    ctx.lineCap = 'round';
+
+    if (cursor.points.length > 0) {
+        ctx.beginPath();
+        ctx.moveTo(cursor.points[0].x, cursor.points[0].y);
+        for (const point of cursor.points) {
+            ctx.lineTo(point.x, point.y);
+        }
+        ctx.stroke();
+    }
+}
+
+// Add clear button
 const clearButton = document.createElement('button');
 app.appendChild(clearButton);
 clearButton.innerText = "Clear";
 clearButton.addEventListener("click", () => {
     const canvas = document.getElementById('canvas') as HTMLCanvasElement;
     if (canvas) {
-        const ctx = canvas.getContext('2d');
         if (ctx) {
             ctx.clearRect(0, 0, canvas.width, canvas.height);
         }
